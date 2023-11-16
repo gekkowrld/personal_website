74c1bed299fea5d588321f9ca2de3f7e9c12021f
Restyled.io <commits@restyled.io>
2023-11-16 21:12:55 +0000

    Restyled by clang-format

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,15 @@
 let api_url = "https://api.quotable.io/random?maxLength=50";
-let quote = fetch(api_url)
-  .then((response) => response.json())
-  .then((data) => {
-    let page_quote = document.querySelector("aside");
-    let q_span = document.createElement("q");
-    q_span.classList.add("quote_q");
-    let a_span = document.createElement("cite");
-    a_span.classList.add("quote_cite");
-    a_span.innerText = data.author;
-    q_span.innerText = data.content;
-    page_quote.appendChild(q_span);
-    page_quote.appendChild(a_span);
-  });
+let quote = fetch(api_url).then((response) => response.json()).then((data) => {
+  let page_quote = document.querySelector("aside");
+  let q_span = document.createElement("q");
+  q_span.classList.add("quote_q");
+  let a_span = document.createElement("cite");
+  a_span.classList.add("quote_cite");
+  a_span.innerText = data.author;
+  q_span.innerText = data.content;
+  page_quote.appendChild(q_span);
+  page_quote.appendChild(a_span);
+});
 
 const name_field = document.querySelector("#name");
 const msg_field = document.querySelector("#msg");
